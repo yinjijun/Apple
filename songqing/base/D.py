c76669fdcc9dd046463120a280c9249c746ff20e
= <854214103@qq.com>
2020-08-19 12:38:14 +0800

    测试

---
+++ songqing/base/D.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+# @project  : Apple
+# @author   : yinjijun
+# @file     : D.py
+# @ide      : PyCharm
+# @time     : 2020/8/17 10:37
+
+# import datetime
+#
+# str1213 = 'werrt\'fffsss\''
+# print(str)
+#
+#
+# # alist = ['i', 'like', 'football'print('*'.join(alist))
+# # print(datetime.datetime.now().month)
+# # print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+# nowyear = datetime.datetime.now().year
+# age = nowyear - int(input('出生'))
+# print(age)
+# res = lambda
+
+#
+# class Rect:
+#     def __init__(self, width, height):
+#         self.width = width
+#         self.height = height
+#
+#     @property
+#     def area(self):
+#         return self.width*self.height
+#
+# rect = Rect(800, 900).area
+#
+# print(rect)
+
+
+import os
+
+print(os.path)
+print(os.name)
+print(os.linesep)
+print(os.sep)
+print(os.getcwd())
+print(os.path.abspath('12.png'))
+print(os.stat('12.png').st_size)
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
